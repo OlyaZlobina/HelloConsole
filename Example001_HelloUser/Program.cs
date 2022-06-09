390d5bf4978f26c663dc2b5be7c4785214063f76
Olya <Zlobinaolya87@gmail.com>
2022-06-09 19:59:21 +0300

    commit HelloUser

---
+++ Example001_HelloUser/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(" Hello! ");
+Console.Write(" Введите имя ");
+Console.Write(" Olya ");
---
+++ exampl001_HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine(" Hello ");
+Console.Write(" Введите ваше имя ");
+Console.Write(" Olya ");
